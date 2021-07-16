9f1632d224870e396d485422abaeff994acb6eed
Valeriy Malyshev <coloreeta@gmail.com>
2021-07-16 11:08:58 +0300

    3 lesson (processing)

--- sources/src/Controller/DefaultController.php
+++ sources/src/Controller/DefaultController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends  AbstractController
 {
     /**
      * @Route ("/", name="default_index")
      */
-    public function indexAction() {
-        echo 'Hello World!';
-        die;
+    public function indexAction() : Response
+    {
+        return $this->render('default/index.html.twig');
     }
 }
--- sources/src/Controller/LuckyController.php
+++ sources/src/Controller/LuckyController.php
@@ -3,22 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
      * @Route ("/lucky/test", name="lucky_test")
      */
-    public function testAction() {
-        echo 'This is the test action in lucky controller.';
-        die;
+    public function testAction() : Response
+    {
+        return $this->render('test.html.twig');
     }
 
     /**
      * @Route ("/lucky/number", name="lucky_number")
      */
-    public function numberAction() {
+    public function numberAction()
+    {
         echo 'Number action';
         die;
+    }
+
+    /**
+     * @Route ("/lucky/index", name="lucky_index")
+     */
+    public function indexAction() : Response
+    {
+        return $this->render('lucky/index.html.twig');
     }
 }
