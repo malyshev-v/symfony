e3866e4a1e4dcbbd89751351af151e94d4efa49a
Valeriy Malyshev <coloreeta@gmail.com>
2021-08-23 19:35:42 +0300

    lesson 5 (almost ended 2)

--- sources/src/Entity/Post.php
+++ sources/src/Entity/Post.php
@@ -37,16 +37,26 @@ class Post
      */
     private $published_by;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -54,11 +64,18 @@ class Post
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    /**
+     * @param string $description
+     * @return $this
+     */
     public function setDescription(string $description): self
     {
         $this->description = $description;
@@ -66,11 +83,18 @@ class Post
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPublishedAt(): ?string
     {
         return $this->published_at;
     }
 
+    /**
+     * @param string $published_at
+     * @return $this
+     */
     public function setPublishedAt(string $published_at): self
     {
         $this->published_at = $published_at;
@@ -78,11 +102,18 @@ class Post
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPublishedBy(): ?string
     {
         return $this->published_by;
     }
 
+    /**
+     * @param string $published_by
+     * @return $this
+     */
     public function setPublishedBy(string $published_by): self
     {
         $this->published_by = $published_by;
