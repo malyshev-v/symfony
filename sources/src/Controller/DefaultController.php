deb3e213e82db5f82687f7daab3affa8a91f8c5e
Valeriy Malyshev <v.malyshev@piogroup.net>
2021-09-14 12:14:19 +0300

    lesson 9 completed

--- sources/src/Controller/DefaultController.php
+++ sources/src/Controller/DefaultController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\FeedbackForm;
 use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 /**
  * Class DefaultController
@@ -45,10 +50,42 @@ class DefaultController extends AbstractController
     /**
      * @Route ("/feedback", name="default_feedback")
      *
+     * @param Request $request
+     * @param MailerInterface $mailer
+     *
      * @return Response
+     *
+     * @throws TransportExceptionInterface
      */
-    public function feedbackAction() : Response
+    public function feedbackAction(Request $request, MailerInterface $mailer) : Response
     {
-        return $this->render('default/feedback.html.twig');
+        $feedbackForm = $this->createForm(FeedbackForm::class);
+        $feedbackForm->handleRequest($request);
+
+        if ($feedbackForm->isSubmitted() && $feedbackForm->isValid()) {
+            $data = $feedbackForm->getData();
+            $email = (new Email())
+                ->from('burm.courses@gmail.com')
+                ->to('v.malyshev@piogroup.net')
+                ->subject('Feedback from Symfony blog')
+                ->text(sprintf('Hi! My name is %s and it is my feedback about your blog! \n
+                    I think the next: %s. If u want to give a question, u can find me by my contacts: %s. Best regards!',
+                    $data['name'], $data['contacts'], $data['description']))
+                ->html(str_replace(
+                    array('SenderName', 'SenderContacts', 'SenderMessage'),
+                    array($data['name'], $data['contacts'], $data['description']),
+                    file_get_contents(__DIR__ . '/../../templates/default/email/feedback.html')));
+
+            $mailer->send($email);
+
+            $this->addFlash(
+                'notice',
+                'Your feedback has been successfully sent!'
+            );
+        }
+
+        return $this->render('default/feedback.html.twig', [
+            'form' => $feedbackForm->createView(),
+        ]);
     }
 }
